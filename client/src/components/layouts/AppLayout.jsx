77ec97f69960ec39ae3617693eee0881aa16febf
Vishal Rawat <omniverserawat2005@gmail.com>
2026-03-22 17:22:29 +0530

    Added the notification component to AppLayout and useProfile hook call.

--- client/src/components/layouts/AppLayout.jsx
+++ client/src/components/layouts/AppLayout.jsx
@@ -1,12 +1,25 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import useProfile from "../../hooks/useProfile";
+import useNotification from "../../hooks/useNotification";
+import Notification from "../ui/Notification";
 
 export default function AppLayout() {
+  const { message, isError, removeNotification } = useNotification();
+  useProfile();
+
   return (
     <>
       <Navbar />
       <main className="flex-1">
+        {message && (
+          <Notification
+            message={message}
+            type={isError ? "error" : "success"}
+            onClose={() => removeNotification()}
+          />
+        )}
         <Outlet />
       </main>
       <Footer />
