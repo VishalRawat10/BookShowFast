7b7b635f9905d630547b726e75091ff55b7d23f5
Vishal Rawat <omniverserawat2005@gmail.com>
2026-03-22 17:34:54 +0530

    Refactor useProfile hook to improve data handling and error management

--- client/src/hooks/useProfile.jsx
+++ client/src/hooks/useProfile.jsx
@@ -1,14 +1,15 @@
+import { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useDispatch } from "react-redux";
 import { setUser } from "../store/authSlice";
-import api from "../services/axios";
+import authApi from "../services/axios";
 
 export default function useProfile() {
   const dispatch = useDispatch();
 
   const getProfile = async () => {
-    const res = await api.get("/auth/profile");
-    return res;
+    const res = await authApi.get("/auth/profile");
+    return res?.data?.user;
   };
 
   const { data, isPending, isError, isSuccess, error } = useQuery({
@@ -18,8 +19,11 @@ export default function useProfile() {
     staleTime: 1000 * 60 * 5,
   });
 
-  if (isSuccess) dispatch(setUser(data.data.user));
-  if (isError) console.log(error);
+  useEffect(() => {
+    if (isSuccess && data) {
+      dispatch(setUser(data));
+    }
+  }, [isSuccess, data, dispatch]);
 
-  return { data, isPending, isError, isSuccess };
+  return { data, isPending, isError, isSuccess, error };
 }
