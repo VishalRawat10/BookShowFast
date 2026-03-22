5636aaa8ba27d176eaa627abd46e86d5248fbf62
Vishal Rawat <omniverserawat2005@gmail.com>
2026-03-22 17:44:27 +0530

    Update OTP generation logic and add getSafeUser utility function

--- server/src/utils/utils.js
+++ server/src/utils/utils.js
@@ -1,3 +1,18 @@
 export const generateOTP = (noOfDigits = 6) => {
-    return Math.floor(10 ** noOfDigits * Math.random()).toString();
+    return Math.floor(10 ** (noOfDigits - 1) * (Math.random() + 1)).toString();
+}
+
+export const getSafeUser = (user) => {
+    if (typeof user !== "object") {
+        user = user.toObject();
+    }
+
+    const safeUser = {
+        email: user.email || null,
+        phone: user.phone || null,
+        name: user.name || null,
+        role: user.role || null,
+        isProfileComplete: user.isProfileComplete
+    }
+    return safeUser;
 }
