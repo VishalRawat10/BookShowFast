bf7d6ff65ec066cd4e4fa87aea5c72560a6a8618
Vishal Rawat <omniverserawat2005@gmail.com>
2026-03-22 17:36:50 +0530

    Add notification reducer to the Redux store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice.js";
+import notificationReducer from "./notificationSlice.js";
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        notification: notificationReducer,
     }
 });
